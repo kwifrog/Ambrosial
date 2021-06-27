c9f49ee7c19eb7f8397480569e86adca59d2ef97
Kiwi <6136683+KiwiOfLesbos@users.noreply.github.com>
2021-06-27 17:41:26 -0400

    Downgrading Forge to the Recommended
    And this is before I try and get a mixin mod working in my dev environment lol

---
+++ src/main/java/com/kiwi/ambrosial/Ambrosial.java
@@ -0,0 +1,28 @@
+package com.kiwi.ambrosial;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Ambrosial.MODID)
+@Mod.EventBusSubscriber(modid = Ambrosial.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Ambrosial
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "ambrosial";
+
+    public Ambrosial() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+}
