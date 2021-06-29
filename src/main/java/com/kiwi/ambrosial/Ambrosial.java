411bd46d64bb256a9b7fcb2c2afa22e0a6e31dc0
Kiwi <6136683+KiwiOfLesbos@users.noreply.github.com>
2021-06-28 21:01:00 -0400

    Fixed Creative Tab Sorting

--- src/main/java/com/kiwi/ambrosial/Ambrosial.java
+++ src/main/java/com/kiwi/ambrosial/Ambrosial.java
@@ -1,24 +1,16 @@
 package com.kiwi.ambrosial;
 
 import com.kiwi.ambrosial.registry.ModItems;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import javax.annotation.Nonnull;
 
 @Mod(Ambrosial.MODID)
 @Mod.EventBusSubscriber(modid = Ambrosial.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Ambrosial
 {
-    public static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "ambrosial";
 
     public static final  AmbrosialItemGroup ITEM_GROUP = new AmbrosialItemGroup(Ambrosial.MODID);
@@ -34,18 +26,5 @@ public class Ambrosial
     private void setup(final FMLCommonSetupEvent event)
     {
 
-    }
-
-    public static class AmbrosialItemGroup extends ItemGroup {
-
-        public AmbrosialItemGroup(String label) {
-            super(label);
-        }
-
-        @Override
-        @Nonnull
-        public ItemStack makeIcon() {
-            return new ItemStack(ModItems.SLICED_BREAD.get());
-        }
     }
 }
---
+++ src/main/java/com/kiwi/ambrosial/AmbrosialItemGroup.java
@@ -0,0 +1,45 @@
+package com.kiwi.ambrosial;
+
+import com.kiwi.ambrosial.registry.ModItems;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("unused")
+public class AmbrosialItemGroup extends ItemGroup {
+
+    public AmbrosialItemGroup(String label) {
+        super(label);
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack makeIcon() {
+        return new ItemStack(ModItems.SLICED_BREAD.get());
+    }
+
+    @Override
+    public void fillItemList(NonNullList<ItemStack> items) {
+
+        registerCrops(items);
+        registerIngredients(items);
+        registerItems(items);
+
+    }
+
+    private void registerCrops(NonNullList<ItemStack> items) {
+        items.add(new ItemStack(ModItems.RADISH.get()));
+        items.add(new ItemStack(ModItems.KIWI.get()));
+    }
+
+    private void registerIngredients(NonNullList<ItemStack> items) {
+        items.add(new ItemStack(ModItems.SLICED_BREAD.get()));
+        items.add(new ItemStack(ModItems.TOAST.get()));
+    }
+
+    private void registerItems(NonNullList<ItemStack> items) {
+        items.add(new ItemStack(ModItems.CANNING_JAR.get()));
+    }
+}
