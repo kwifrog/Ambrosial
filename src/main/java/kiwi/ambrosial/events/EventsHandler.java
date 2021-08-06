ffc8db9012564cc4719ae30b5069877155e79894
Kiwi <6136683+KiwiOfLesbos@users.noreply.github.com>
2021-08-06 00:56:34 -0400

    Todo

--- src/main/java/kiwi/ambrosial/events/EventsHandler.java
+++ src/main/java/kiwi/ambrosial/events/EventsHandler.java
@@ -3,7 +3,6 @@ package kiwi.ambrosial.events;
 import net.minecraftforge.common.MinecraftForge;
 
 public class EventsHandler {
-
     public static void setupEvents() {
         MinecraftForge.EVENT_BUS.register(new TemptationEvent());
     }
--- src/main/java/kiwi/ambrosial/setup/Compostables.java
+++ src/main/java/kiwi/ambrosial/setup/Compostables.java
@@ -1,6 +1,7 @@
 package kiwi.ambrosial.setup;
 
 public class Compostables {
+    //TODO Add foods to compostables
     public static void register() {
 
     }
